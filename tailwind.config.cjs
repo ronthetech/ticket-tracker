2a3320dac4d493fee47a6cbf17c17cf35dd0f89b
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-24 23:22:46 -0500

    style: add container theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   darkMode: ["class"],
   content: ["pages/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+      screens: {
+        "2xl": "1360px",
+      },
+    },
     extend: {},
   },
   plugins: [],
