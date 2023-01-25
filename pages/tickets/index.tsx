d9f1e76660bb5c3088fa3edca9dba5ba8b1d7132
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-25 18:15:31 -0500

    edit: add getServerSideProps

--- pages/tickets/index.tsx
+++ pages/tickets/index.tsx
@@ -3,42 +3,56 @@ import TicketList from "components/tickets/TicketList"
 import Head from "next/head"
 import type { TicketT } from "types/ticket"
 
-const tickets: TicketT[] = [
-  {
-    _id: "1",
-    subject: "lagging",
-    description: "Mouse events are lagging",
-    severity: "Low",
-    assignee: "Alan",
-    status: "Open",
-  },
-  {
-    _id: "2",
-    subject: "crash",
-    description: "Computer crashes when I open program",
-    severity: "High",
-    assignee: "Luke",
-    status: "Open",
-  },
-  {
-    _id: "3",
-    subject: "no notifications",
-    description: "Not receiving my notifications",
-    severity: "Medium",
-    assignee: "Gail",
-    status: "Closed",
-  },
-  {
-    _id: "4",
-    subject: "form submit",
-    description: "i cant submit my form entry",
-    severity: "Low",
-    assignee: "Ernie",
-    status: "Open",
-  },
-]
+// const tickets: TicketT[] = [
+//   {
+//     _id: "1",
+//     subject: "lagging",
+//     description: "Mouse events are lagging",
+//     severity: "Low",
+//     assignee: "Alan",
+//     status: "Open",
+//   },
+//   {
+//     _id: "2",
+//     subject: "crash",
+//     description: "Computer crashes when I open program",
+//     severity: "High",
+//     assignee: "Luke",
+//     status: "Open",
+//   },
+//   {
+//     _id: "3",
+//     subject: "no notifications",
+//     description: "Not receiving my notifications",
+//     severity: "Medium",
+//     assignee: "Gail",
+//     status: "Closed",
+//   },
+//   {
+//     _id: "4",
+//     subject: "form submit",
+//     description: "i cant submit my form entry",
+//     severity: "Low",
+//     assignee: "Ernie",
+//     status: "Open",
+//   },
+// ]
 
-const TicketsHome = () => {
+const getTickets = (): Promise<TicketT[]> =>
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  fetch("https://rjf-manager-server.onrender.com/tickets").then((response) =>
+    response.json()
+  )
+
+export const getServerSideProps = async () => {
+  // const res = await fetch("https://rjf-manager-server.onrender.com/tickets")
+
+  return {
+    props: { initialData: await getTickets() },
+  }
+}
+
+function TicketsHome({ initialData }: { initialData: TicketT[] }) {
   return (
     <Layout>
       <Head>
@@ -52,11 +66,12 @@ const TicketsHome = () => {
 
         <article className="my-3 rounded border bg-slate-400/20 p-5 lg:my-8">
           <ul>
-            <TicketList tickets={tickets} />
+            <TicketList tickets={initialData} />
           </ul>
         </article>
       </section>
     </Layout>
   )
 }
+
 export default TicketsHome
