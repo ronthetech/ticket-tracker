41784dc2a8cb84f1b2303aca27c35eacc29f8c00
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-24 23:28:52 -0500

    edit: add styling and update to ts file extension

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="min-h-screen bg-slate-300">
         <Main />
         <NextScript />
       </body>
