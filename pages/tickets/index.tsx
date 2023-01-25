9f14e753ad58ea5bc9ae2c37ed537f163554d15a
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-25 12:23:51 -0500

    feat: add tickets home page

---
+++ pages/tickets/index.tsx
@@ -0,0 +1,46 @@
+import { Layout } from "components/layout.tsx"
+import TicketList from "components/tickets/TicketList"
+import Head from "next/head"
+import type { TicketT } from "types/ticket"
+
+const tickets: TicketT[] = [
+  {
+    _id: "1",
+    subject: "lagging",
+    description: "Mouse events are lagging",
+    severity: "Low",
+    assignee: "Alan",
+    status: "Open",
+  },
+  {
+    _id: "2",
+    subject: "crash",
+    description: "Computer crashes when I open program",
+    severity: "High",
+    assignee: "Luke",
+    status: "Open",
+  },
+]
+
+const TicketsHome = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Ticket Tracker</title>
+        <meta name="description" content="Ticket Tracker" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="my-4 mx-auto max-w-3xl rounded p-3">
+        <h1 className="text-red-800">Tickets</h1>
+
+        <article className="my-3 rounded border bg-slate-400/20 p-5 lg:my-8">
+          <ul>
+            <TicketList tickets={tickets} />
+          </ul>
+        </article>
+      </section>
+    </Layout>
+  )
+}
+export default TicketsHome
