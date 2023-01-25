e2d96c5a72c44d374b932338fe88e505ef5ffe06
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-25 00:33:06 -0500

    edit: add theme provider, add type and update file extension

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css"
+
+import { ThemeProvider } from "next-themes"
+import type { AppProps } from "next/app"
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
