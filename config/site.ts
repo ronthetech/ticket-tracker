3b2a0063cb94c45eca8c96d52cd28562871448d0
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-24 23:26:34 -0500

    feat: add site config

---
+++ config/site.ts
@@ -0,0 +1,27 @@
+import type { NavItem } from "types/nav"
+
+interface SiteConfig {
+  name: string
+  description: string
+  mainNav: NavItem[]
+  links: {
+    twitter: string
+    github: string
+  }
+}
+
+export const siteConfig: SiteConfig = {
+  name: "Next.js",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/ronjtech",
+    github: "https://github.com/ronthetech",
+  },
+}
