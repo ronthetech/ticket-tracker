66068a933c84a648a1d66108577ebf248ce3f578
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-25 20:18:11 -0500

    feat: add auth slot

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,5 +1,6 @@
 import { siteConfig } from "config/site"
 import Link from "next/link"
+import AuthSlot from "./AuthSlot"
 
 import { Icons } from "./icons"
 import { MainNav } from "./main-nav"
@@ -12,6 +13,7 @@ export function SiteHeader() {
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-4">
+            <AuthSlot />
             <Link
               href={siteConfig.links.github}
               target="_blank"
