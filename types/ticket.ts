896930ba2abffdbc2af201543b188f02eab27f53
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-26 19:43:29 -0500

    edit: add Ticket type

--- types/ticket.ts
+++ types/ticket.ts
@@ -13,3 +13,14 @@ export type TicketT = {
   assignee: string
   status: string
 }
+
+export type Ticket = {
+  id: string
+  createdAt: Date
+  userId: string
+  subject: string
+  description: string
+  severity: string
+  assignee: string
+  status: boolean
+}
