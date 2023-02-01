061c087af7777fb543b727d6c15595574321e687
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-31 19:05:19 -0500

    edit: add url for dev testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://trackerx.vercel.app",
+    baseUrl: "http://localhost:3000/",
+    // baseUrl: "https://trackerx.vercel.app",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
