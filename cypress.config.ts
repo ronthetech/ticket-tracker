718b8e2603d05f607cf121403eae5d3ed4954949
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-31 19:29:56 -0500

    add production url

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000/",
-    // baseUrl: "https://trackerx.vercel.app",
+    // baseUrl: "http://localhost:3000/",
+    baseUrl: "https://trackerx.vercel.app",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
